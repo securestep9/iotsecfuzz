af478a83d946d1211c11a1642d038ff1c24c98be
Drakylar Drakylar <drakylar@gmail.com>
2018-09-19 21:19:50 +0000

    Update gui.py

--- modules/hardware/gui.py
+++ modules/hardware/gui.py
@@ -1,1 +1,19 @@
 #interface functions
+
+
+def loadAll(screen):
+    
+    
+    loadButtons(screen)
+    loadTexts(screen)
+    
+    
+##################################
+
+
+def loadButtons(screen):
+    
+    addbutton(screen,x,y,text)
+    addbutton(screen,x,y,text)
+    ...
+    
